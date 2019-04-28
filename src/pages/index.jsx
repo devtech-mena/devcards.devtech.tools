6712334e261606f4dcd9af5cb797a32124f71cfa
Ammar Lakis <ammar@devtech.tools>
2019-04-28 19:12:54 +0300

    style: add uikit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import Search from '../components/Search';
+import React, { Component } from 'react';
+import Filter from '../components/Filter';
 import Card from '../components/Card';
 import '../assets/main.scss';
 
-export default () =>
-    <div className="container">
-        <div className="item">
-            <Search></Search>
-        </div>
-        <div className="item">
-            <Card></Card>
-        </div>
-    </div>
+class IndexWrapper extends Component {
+    state = { ready: false };
+    componentDidMount = () => {
+        if (typeof window !== 'undefined') {
+            const uikit = require('uikit');
+            const icons = require('uikit/dist/js/uikit-icons.min');
+            uikit.use(icons);
+            this.setState({ ready: true });
+        }
+    };
+
+    render() {
+        return (
+            <div dir="rtl">
+                <Filter></Filter>
+                <Card></Card>
+            </div>
+        );
+    }
+}
+
+export default IndexWrapper
