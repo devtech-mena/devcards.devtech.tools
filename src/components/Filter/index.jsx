19a8329328082bdbc6b1b614cb5302f494008142
Ammar Lakis <ammar@devtech.tools>
2019-04-29 04:14:12 +0300

    style: make filters sticky

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 class Filter extends React.Component {
     render() {
         const items = [];
-        const skills = ['android', 'ios'];
+        const skills = ['android', 'ios', 'web'];
         // const skills = this.props.data.site.siteMetadata.skills;
         skills.forEach(skill => {
-            items.push(<li uk-filter-control={"." + skill} key={"filter-" + skill}><a href="#">{skill}</a></li>)
+            items.push(<li uk-filter-control={"." + skill} key={"filter-" + skill}><a href="/#">{skill}</a></li>)
         });
         return (
-            <ul className="uk-subnav uk-subnav-pill">
+            <ul className="uk-subnav uk-subnav-pill" uk-sticky="show-on-up: true">
                 {items}
             </ul>
         );
