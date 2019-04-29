f6654a9659ae5ccc51638ed5ea5f1018dc58acd6
Ammar Lakis <ammar@devtech.tools>
2019-04-29 04:18:10 +0300

    style: add skills badges and description to cards

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -16,19 +16,43 @@ class CardList extends React.Component {
                 name: 'user3',
                 skills: ['web'],
             },
+            {
+                name: 'user4',
+                skills: ['web'],
+            },
+            {
+                name: 'user5',
+                skills: ['web'],
+            },
+            {
+                name: 'user6',
+                skills: ['web'],
+            },
         ];
         users.forEach(user => {
+            const userSkills = [];
+            user.skills.forEach(skill => {
+                userSkills.push(
+                    <span key={user.name + skill} className="uk-label">{skill}</span>
+                );
+            });
             items.push(
-                <li key={user.name} className={user.skills.join(" ")}>
-                    <div className="uk-card uk-card-default uk-card-body">{user.name}</div>
-                </li>
+                <div key={user.name} className={user.skills.join(" ")}>
+                    <div className="uk-card uk-card-default uk-card-body uk-width-xxlarge uk-margin">
+                        <span className="uk-card-badge">
+                            {userSkills}
+                        </span>
+                        <h3 className="uk-card-title">{user.name}</h3>
+                        <p>User {user.name} has skills {user.skills}</p>
+                    </div>
+                </div>
             );
         });
 
         return (
-            <ul className="js-filter uk-child-width-1-2 uk-child-width-1-3@m uk-text-center" uk-grid="">
+            <div className="js-filter uk-text-center uk-margin-xlarge-top">
                 {items}
-            </ul>
+            </div>
         )
     }
 }
