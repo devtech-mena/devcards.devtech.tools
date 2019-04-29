0021b495175cb565370a504adaa342b63ac8429b
Ammar Lakis <ammar@devtech.tools>
2019-04-29 05:39:25 +0300

    style: add image to card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -9,12 +9,21 @@ class Card extends React.Component {
             );
         });
         return (
-            <div className="uk-card uk-card-default uk-card-body uk-width-1-2@l uk-margin">
-                <span className="uk-card-badge">
-                    {userSkills}
-                </span>
-                <h3 className="uk-card-title">{this.props.user.name}</h3>
-                <p>User {this.props.user.name} has skills {this.props.user.skills}</p>
+            <div className="uk-card uk-card-default uk-grid-collapse uk-width-1-2@l uk-margin" uk-grid="">
+                <div className="uk-card-media-left uk-cover-container uk-width-1-4@m">
+                    <img src="https://getuikit.com/docs/images/light.jpg" alt="" uk-cover="" />
+                    <canvas width="600" height="400"></canvas>
+                </div>
+                <div className="uk-card-body uk-width-3-4@m uk-text-left">
+                    <span className="uk-card-badge">
+                        {userSkills}
+                    </span>
+                    <h3 className="uk-card-title">{this.props.user.name}</h3>
+                    <p>User {this.props.user.name} has skills {this.props.user.skills}</p>
+                </div>
+                <div>
+
+                </div>
             </div>
         );
     }
