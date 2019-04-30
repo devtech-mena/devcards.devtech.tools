24c4dd1ada48a629c64100f823349aaa073384e5
Ammar Lakis <ammar@devtech.tools>
2019-04-30 12:21:07 +0300

    fix: add missing phonenumber icon

---
+++ src/assets/icons/index.js
@@ -0,0 +1,16 @@
+import codepen from './codepen.svg';
+import email from './email.svg';
+import github from './github.svg';
+import gitlab from './gitlab.svg';
+import stackoverflow from './stackoverflow.svg';
+import website from './website.svg';
+import phonenumber from './phonenumber.svg';
+export default {
+    codepen,
+    email,
+    github,
+    gitlab,
+    stackoverflow,
+    website,
+    phonenumber
+}
